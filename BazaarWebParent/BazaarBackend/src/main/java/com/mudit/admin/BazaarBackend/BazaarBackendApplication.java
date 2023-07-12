3864d8bcaf35129c1501c48d39ca40f88ea6db60
Deadlegend1321 <mudit1318@outlook.com>
2023-07-12 12:20:24 +0530

    Initial Setup and Admin Control Page Homepage Bootstrap

---
+++ BazaarWebParent/BazaarBackend/src/main/java/com/mudit/admin/BazaarBackend/BazaarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mudit.admin.BazaarBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazaarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazaarBackendApplication.class, args);
+	}
+
+}
---
+++ BazaarWebParent/BazaarFrontend/src/main/java/com/mudit/site/BazaarFrontend/BazaarFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.mudit.site.BazaarFrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazaarFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazaarFrontendApplication.class, args);
+	}
+
+}
