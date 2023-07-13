b4d97a7e24020ce53e704f9c7c3e471b1e82c96f
Deadlegend1321 <mudit1318@outlook.com>
2023-07-13 20:31:11 +0530

    Added Functionality of adding a new User through Form, Showing it and encoding password

---
+++ BazaarWebParent/BazaarBackend/src/main/java/com/mudit/admin/security/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.mudit.admin.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+	
+	@Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+	 @Bean
+	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		 http
+		    .authorizeHttpRequests((authorize) -> authorize
+		        .anyRequest().permitAll()
+		    );
+	        return http.build();
+	    }
+}
--- BazaarWebParent/BazaarBackend/src/main/java/com/mudit/admin/user/UserService.java
+++ BazaarWebParent/BazaarBackend/src/main/java/com/mudit/admin/user/UserService.java
@@ -3,18 +3,40 @@ package com.mudit.admin.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.mudit.common.entity.Role;
 import com.mudit.common.entity.User;
 
 @Service
 public class UserService {
 	
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	public List<User> listAll() {
-		return (List<User>) repo.findAll();
+		return (List<User>) userRepository.findAll();
+	}
+	
+	public List<Role> listRoles(){
+		return (List<Role>) roleRepository.findAll();
+	}
+
+	public void save(User user) {
+		encodePassword(user);
+		userRepository.save(user);
+	}
+	
+	private void encodePassword(User user) {
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
 	}
 
 }
