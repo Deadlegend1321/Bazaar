8c287f6dc3f6ee7c5fbc99e6e4d5953a1afdb322
Deadlegend1321 <mudit1318@outlook.com>
2023-07-18 19:47:19 +0530

    Made UI Responsive

--- BazaarCommon/src/main/java/com/mudit/common/entity/User.java
+++ BazaarCommon/src/main/java/com/mudit/common/entity/User.java
@@ -71,4 +71,9 @@ public class User {
 		if(id == null || photos == null) return "/images/default-user.png";
 		return "/user-photos/" + this.id + "/" + this.photos;
 	}
+	
+	@Transient
+	public String getFullName() {
+		return firstName + " " + lastName;
+	}
 }
