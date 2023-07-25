4ad635a7999f2c842289592fed221a7b0cef3ecf
Deadlegend1321 <mudit1318@outlook.com>
2023-07-25 17:06:38 +0530

    Brand Unique, Sort, Filter & Pagination

--- BazaarWebParent/BazaarBackend/src/main/java/com/mudit/admin/brand/BrandRepository.java
+++ BazaarWebParent/BazaarBackend/src/main/java/com/mudit/admin/brand/BrandRepository.java
@@ -1,5 +1,8 @@
 package com.mudit.admin.brand;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import com.mudit.common.entity.Brand;
@@ -7,5 +10,10 @@ import com.mudit.common.entity.Brand;
 public interface BrandRepository extends CrudRepository<Brand, Integer>, PagingAndSortingRepository<Brand, Integer> {
 	
 	public Long countById(Integer id);
+	
+	public Brand findByName(String name);
+	
+	@Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
+	public Page<Brand> findAll(String keyword, Pageable pageable);
 
 }
--- BazaarWebParent/BazaarBackend/src/main/java/com/mudit/admin/brand/BrandService.java
+++ BazaarWebParent/BazaarBackend/src/main/java/com/mudit/admin/brand/BrandService.java
@@ -1,15 +1,21 @@
 package com.mudit.admin.brand;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import java.util.NoSuchElementException;
 
 import com.mudit.common.entity.Brand;
 
 @Service
 public class BrandService {
+	public static final int BRANDS_PER_PAGE = 10;
+	
 	@Autowired
 	private BrandRepository repo;
 	
@@ -17,10 +23,24 @@ public class BrandService {
 		return (List<Brand>) repo.findAll();
 	}
 	
+	public Page<Brand> listByPage(int pageNum, String sortField, String sortDir, String keyword) {
+		Sort sort = Sort.by(sortField);
+		
+		sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+				
+		Pageable pageable = PageRequest.of(pageNum - 1, BRANDS_PER_PAGE, sort);
+		
+		if (keyword != null) {
+			return repo.findAll(keyword, pageable);
+		}
+		
+		return repo.findAll(pageable);		
+	}
+	
 	public Brand save(Brand brand) {
 		return repo.save(brand);
 	}
-
+	
 	public Brand get(Integer id) throws BrandNotFoundException {
 		try {
 			return repo.findById(id).get();
@@ -28,14 +48,29 @@ public class BrandService {
 			throw new BrandNotFoundException("Could not find any brand with ID " + id);
 		}
 	}
-
+	
 	public void delete(Integer id) throws BrandNotFoundException {
 		Long countById = repo.countById(id);
-
+		
 		if (countById == null || countById == 0) {
 			throw new BrandNotFoundException("Could not find any brand with ID " + id);			
 		}
-
+		
 		repo.deleteById(id);
+	}
+	
+	public String checkUnique(Integer id, String name) {
+		boolean isCreatingNew = (id == null || id == 0);
+		Brand brandByName = repo.findByName(name);
+		
+		if (isCreatingNew) {
+			if (brandByName != null) return "Duplicate";
+		} else {
+			if (brandByName != null && brandByName.getId() != id) {
+				return "Duplicate";
+			}
+		}
+		
+		return "OK";
 	}
 }
